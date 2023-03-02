a9eda50195d83f7754cdd5c91b807cbdc120ae2c
tauhid880 <tauhid880@gmail.com>
2023-03-03 05:37:18 +0600

    message & schedule api added

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ async function run() {
     const scheduleDataCollection = client
       .db("scheduleApp")
       .collection("scheduleData");
+    const messagesCollection = client.db("scheduleApp").collection("Messages");
 
     //create users
     app.post("/users", async (req, res) => {
@@ -40,11 +41,35 @@ async function run() {
       res.send(users);
     });
 
-    // post Data
-    app.post("/data", async (req, res) => {
+    // post schedule Data
+    app.post("/schedules", async (req, res) => {
       const data = req.body;
       const result = await scheduleDataCollection.insertMany(data);
       res.send(result);
+    });
+
+    // get schedule Data
+    app.get("/schedules", async (req, res) => {
+      const Faculty_Email = req.query.Faculty_Email;
+      const query = { Faculty_Email };
+      const schedules = await scheduleDataCollection.find(query).toArray();
+      res.send(schedules);
+    });
+
+    // Post messages
+    app.post("/messages", async (req, res) => {
+      const messages = req.body;
+      // const createdAt = new Date();
+      const result = await messagesCollection.insertOne(messages);
+      res.send(result);
+    });
+
+    //get user-wise messages
+    app.get("/messages", async (req, res) => {
+      // const email = req.query.email;
+      const query = {};
+      const messages = await messagesCollection.find(query).toArray();
+      res.send(messages);
     });
   } finally {
   }
